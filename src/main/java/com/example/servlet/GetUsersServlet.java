ed816b94592676c8f3667f74e07500639d54e153
dailar <dailarse00@gmail.com>
2023-02-28 06:03:36 +0300

    replaced dispatcher source

--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -13,10 +13,12 @@ import java.util.Set;
 
 @WebServlet("/users")
 public class GetUsersServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Set<User> users = Warehouse.getInstance().getUsers();
         req.setAttribute("users", users);
-        req.getRequestDispatcher("/jsp/users.jsp").forward(req, resp);
+        //req.getRequestDispatcher("/jsp/users.jsp").forward(req, resp);
+        getServletContext().getRequestDispatcher("/jsp/users.jsp").forward(req, resp);
     }
 }
